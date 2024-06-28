7c83d1632553d76b7c6b11565d227b0fac4f2de2
otaxhu <oscarperniamoreno@gmail.com>
2024-06-28 17:49:47 -0400

    private include_dir to pi-ulid-core target

--- include.cmake
+++ include.cmake
@@ -5,4 +5,4 @@ set(pi-ulidcore_SRCS ${PI_ULID_DIR}/pi-ulid.c)
 
 add_library(pi-ulid-core EXCLUDE_FROM_ALL ${pi-ulidcore_SRCS})
 target_compile_options(pi-ulid-core PRIVATE ${PI_ULID_CFLAGS})
-target_include_directories(pi-ulid-core ${PI_ULID_DIR}/include)
+target_include_directories(pi-ulid-core PRIVATE ${PI_ULID_DIR}/include)
